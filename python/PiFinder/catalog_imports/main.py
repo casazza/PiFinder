3b24dbefd0f6460583fb3f889337bac41b0508f5
Mark Casazza <markcasazza@gmail.com>
2026-03-03 20:02:17 -0500

    First pass at Lynga catalog loader

--- python/PiFinder/catalog_imports/main.py
+++ python/PiFinder/catalog_imports/main.py
@@ -15,30 +15,31 @@ from .database import init_shared_database
 # Loader registry - import functions dynamically to reduce coupling
 CATALOG_LOADERS = [
     # Core catalogs (order matters for referencing)
-    ("steinicke_loader", "load_ngc_catalog"),
+    #MC#("steinicke_loader", "load_ngc_catalog"),
     # Additional catalogs
-    ("caldwell_loader", "load_caldwell"),
-    ("specialized_loaders", "load_collinder"),
-    ("specialized_loaders", "load_taas200"),
-    ("herschel_loader", "load_herschel400"),
-    ("sac_loaders", "load_sac_asterisms"),
-    ("sac_loaders", "load_sac_multistars"),
-    ("sac_loaders", "load_sac_redstars"),
-    ("bright_stars_loader", "load_bright_stars"),
-    ("specialized_loaders", "load_egc"),
-    ("specialized_loaders", "load_rasc_double_Stars"),
-    ("specialized_loaders", "load_barnard"),
-    ("specialized_loaders", "load_sharpless"),
-    ("specialized_loaders", "load_abell"),
-    ("specialized_loaders", "load_arp"),
-    ("specialized_loaders", "load_tlk_90_vars"),
-    ("wds_loader", "load_wds"),
-    ("harris_loader", "load_harris"),
+    #MC#("caldwell_loader", "load_caldwell"),
+    #MC#("specialized_loaders", "load_collinder"),
+    #MC#("specialized_loaders", "load_taas200"),
+    #MC#("herschel_loader", "load_herschel400"),
+    #MC#("sac_loaders", "load_sac_asterisms"),
+    #MC#("sac_loaders", "load_sac_multistars"),
+    #MC#("sac_loaders", "load_sac_redstars"),
+    #MC#("bright_stars_loader", "load_bright_stars"),
+    #MC#("specialized_loaders", "load_egc"),
+    #MC#("specialized_loaders", "load_rasc_double_Stars"),
+    #MC#("specialized_loaders", "load_barnard"),
+    #MC#("specialized_loaders", "load_sharpless"),
+    #MC#("specialized_loaders", "load_abell"),
+    #MC#("specialized_loaders", "load_arp"),
+    #MC#("specialized_loaders", "load_tlk_90_vars"),
+    #MC#("wds_loader", "load_wds"),
+    #MC#("harris_loader", "load_harris"),
+    ("lynga_loader", "load_lynga"),
 ]
 
 POST_PROCESSING_FUNCTIONS = [
     ("post_processing", "fix_object_types"),
-    ("post_processing", "add_missing_messier_objects"),
+    #MC#("post_processing", "add_missing_messier_objects"),
 ]
 
 
@@ -85,7 +86,7 @@ def main():
     objects_db, _ = init_shared_database()
 
     logging.info("creating catalog tables")
-    objects_db.destroy_tables()
+    #MC#objects_db.destroy_tables()
     objects_db.create_tables()
 
     logging.info("loading catalogs")
